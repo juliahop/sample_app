133b90a4ae52f7e7063ac6aefa089389ba95772c
juliahop <hop.julia@hotmail.com>
2020-03-13 21:21:08 +0100

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  resources :users # automatically ensures that the Rails application responds to the RESTful URLs, bv. ensures that a POST request to /users is handled by the create action
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
