05c5f065596bf574767889b8bf11bf33a009aa33
juliahop <hop.julia@hotmail.com>
2020-04-02 21:45:52 +0200

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users # automatically ensures that the Rails application responds to the RESTful URLs, bv. ensures that a POST request to /users is handled by the create action
-  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
