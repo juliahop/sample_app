862ba783b371f4c98ff16c39522964226622bd59
juliahop <hop.julia@hotmail.com>
2021-01-16 22:35:17 +0100

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,15 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  resources :users # automatically ensures that the Rails application responds to the RESTful URLs, bv. ensures that a POST request to /users is handled by the create action
+  # automatically ensures that the Rails application responds to the RESTful URLs, bv. ensures that a POST request to /users is handled by the create action
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
